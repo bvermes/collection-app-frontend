303ec15b4b234ab3ff65202bbefb490c041cd939
bvermes <qerix8787@gmail.com>
2022-04-25 02:51:55 +0200

    picture appeares

--- src/components/collectionlist/CollectibleItem.js
+++ src/components/collectionlist/CollectibleItem.js
@@ -4,10 +4,10 @@ import "bootstrap/dist/css/bootstrap.css";
 import { toBeInTheDocument } from "@testing-library/jest-dom/dist/matchers";
 import { RiCloseCircleLine } from "react-icons/ri";
 import { TiEdit } from "react-icons/ti";
+import Card from "react-bootstrap/Card";
 
 export default function CollectibleItem({
   elements,
-  forSale,
   removeElement,
   updateElement,
 }) {
@@ -37,22 +37,38 @@ export default function CollectibleItem({
       price: "",
     });
   };
+
+  const handleCheckboxChange = (e) => {};
+
   if (edit.id) {
     return <CollectibleFilter edit={edit} onSubmit={submitUpdate} />;
   }
   return elements.map((element, index) => (
     <div className="d-flex element-row" key={index}>
-      <div className="col-3 p-2">HELLO</div>
-      <div className="col-8 p-2">
-        <div className="row">
-          <div className="col">name</div>
-          <div className="col">price</div>
-          <div className="col">value</div>
+      <Card className="bg-dark text-white">
+        <Card.Img src="ronaldinho.jpg" height={200} alt="Card image" />
+      </Card>
+      <div className="row col-8 p-2">
+        <div className="col-10 p-2">
+          <div className="row">
+            <div className="col">name</div>
+            <div className="col">price</div>
+            <div className="col">value</div>
+          </div>
+          <div className="row" key={element.id}>
+            <div className="col">{element.name}</div>
+            <div className="col">{element.price}</div>
+            <div className="col">{element.value}</div>
+          </div>
         </div>
-        <div className="row" key={element.id}>
-          <div className="col">{element.name}</div>
-          <div className="col">{element.price}</div>
-          <div className="col">{element.value}</div>
+        <div className="col-2 mx-auto">
+          <div>For Sale</div>
+          <input
+            type="checkbox"
+            name="name"
+            className="collectible-inputcheckbox"
+            onChange={handleCheckboxChange}
+          />
         </div>
       </div>
 
